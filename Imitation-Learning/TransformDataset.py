caaffeb606ea7c600d14810095892aa230a3eb20
Parth Jindal <pmjindal@gmail.com>
2021-07-31 16:32:41 +0530

    Update IL dataset with mean and std_dev

--- Imitation-Learning/TransformDataset.py
+++ Imitation-Learning/TransformDataset.py
@@ -3,7 +3,9 @@ import glob
 import regex as re
 import json
 import os
+from tqdm import tqdm
 from typing import List
+import numpy as np
 
 
 class Traj2JSON():
@@ -22,7 +24,11 @@ class Traj2JSON():
             os.path.join(root_dir, f'*{system}*.csv'))
 
         self.states = None
+        self.state_norms = None
+
         self.actions = None
+        self.action_norms = None
+
         self.targets = ['X', 'Y']
         self.traj: list = []
         self.metainfo = None
@@ -30,13 +36,18 @@ class Traj2JSON():
         ...
 
     def read(self):
-        for traj_file in self.traj_files:
+        for i, traj_file in enumerate(self.traj_files):
             df = pd.read_csv(traj_file, delimiter=',')
 
             if not self.states:
                 self.states = [x for x in df.columns if self.isState(x)]
+                self.state_norms = np.zeros(
+                    (len(self.states), 2))
+
             if not self.actions:
                 self.actions = [x for x in df.columns if self.isAction(x)]
+                self.action_norms = np.zeros(
+                    (len(self.actions), 2))
 
             if not self.metainfo:
                 self.metainfo = {'System': self.system,
@@ -54,8 +65,49 @@ class Traj2JSON():
                     "action": action
                 })
 
+            N = len(df) - 1
+            # iterative mean and std_dev calculation to prevent over/underflow
+            self.state_norms[:,
+                             0] += (np.sum(df[self.states].to_numpy()[:-1] / N, axis=0) - self.state_norms[:, 0]) / (i+1)
+            self.state_norms[:,
+                             1] += (np.sum(np.square(df[self.states].to_numpy()[:-1]) / N, axis=0) - self.state_norms[:, 1]) / (i+1)
+
+            self.action_norms[:,
+                              0] += (np.sum(df[self.actions].to_numpy()[:-1] / N, axis=0) - self.action_norms[:, 0]) / (i+1)
+            self.action_norms[:,
+                              1] += (np.sum(np.square(df[self.actions].to_numpy()[:-1]) / N, axis=0) - self.action_norms[:, 1]) / (i+1)
+
+        self.state_norms[:, 1] = np.sqrt(self.state_norms[:, 1] -
+                                         np.square(self.state_norms[:, 0]))
+        self.action_norms[:, 1] = np.sqrt(self.action_norms[:, 1] -
+                                          np.square(self.action_norms[:, 0]))
         # append length in meta info
         self.traj[0]['Length'] = len(self.traj)-1  # -1 for meta info
+
+        # print(self.state_norms, self.action_norms)
+        for i, state in enumerate(self.traj[0]['States']):
+            if state in ['Xt', 'Yt']:
+                # same mean and std_dev as X,Y (may change later as Xt,Yt are 1 timestep ahead)
+                index = next(x for x in range(len(self.states))
+                             if self.states[x] == state.strip('t'))
+                self.traj[0]['States'][i] = {
+                    'Name': state,
+                    'mean': self.state_norms[index, 0],
+                    'std': self.state_norms[index, 1]
+                }
+            else:
+                self.traj[0]['States'][i] = {
+                    'Name': state,
+                    'mean': self.state_norms[i, 0],
+                    'std': self.state_norms[i, 1]
+                }
+
+        for i, action in enumerate(self.traj[0]['Actions']):
+            self.traj[0]['Actions'][i] = {
+                'Name': action,
+                'mean': self.action_norms[i, 0],
+                'std': self.action_norms[i, 1]
+            }
 
     def isState(self, x):
         return re.match(r'd?X\d?|d?Y\d?', x)
@@ -69,7 +121,7 @@ class Traj2JSON():
 
 
 def main(systems):
-    for system in systems:
+    for system in tqdm(systems, desc='Converting to JSON ...'):
         traj = Traj2JSON(system, '../traj_data')
         traj.read()
         traj.write(f'dataset/{system}.json')
